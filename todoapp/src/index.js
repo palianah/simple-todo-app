457b2be536dec4612880459ce4e3318b022c7964
Palian Schulz <palian928@gmail.com>
2018-11-30 20:47:45 +0100

    add layout

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Dashboard from "./Dashboard";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
+
+serviceWorker.register();
